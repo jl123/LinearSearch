05af033b71a5ca875aec9cb17ee4013eae933f36
Joseph <jlaos1234@yahoo.com>
2018-09-05 17:28:54 -0700

    Added ArrayList and some streams

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,12 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+
+//takes user input and finds instances of that value in array and ArrayList
+// with random values
 
 public class LinearSearch
 {
+
+   //traverses array provided and returns ArrayList of all indexes where
+   // target int is found
    public static List<Integer> findIndexes(int[] nums, final int TARGET)
+         throws TargetNotFoundException
    {
       List<Integer> indexesOfTarget = new ArrayList<>();
+
       for (int i = 0; i < nums.length; i++)
       {
          if (nums[i] == TARGET)
@@ -14,37 +21,123 @@ public class LinearSearch
             indexesOfTarget.add(i);
          }
       }
+      if (indexesOfTarget.size() == 0)
+      {
+         throw new TargetNotFoundException("No instances of "
+               + TARGET + " found.");
+      }
       return indexesOfTarget;
    }
 
-   private static int[] getRandomArray(int size, int bound)
+   public static List<Integer> findIndexes(List<Integer> nums, final int TARGET)
+         throws TargetNotFoundException
    {
-      int[] nums = new int[size];
+      int[] listAsArray = nums.stream().mapToInt(x -> x).toArray();
+      return findIndexes(listAsArray, TARGET);
+   }
+
+   //produces & returns array of size and high bound value provided
+   private static int[] getRandomArray(final int SIZE, final int BOUND)
+   {
+      int[] nums = new int[SIZE];
       Random ran = new Random();
 
-      for (int i = 0; i < size; i++)
+      for (int i = 0; i < SIZE; i++)
       {
-         nums[i] = ran.nextInt(bound);
+         nums[i] = ran.nextInt(BOUND ) + 1;
       }
       return nums;
    }
 
+   private static List<Integer> getRandomArrayList(final int SIZE, final int BOUND)
+   {
+      return new Random()
+            .ints(SIZE,1,BOUND + 1)
+            .boxed()
+            .collect(Collectors.toCollection(ArrayList::new));
+   }
+
    public static void main(String[] args)
    {
-      final int TARGET = 5;
+      final int SIZE_OF_ARRAY = 75;
+      final int HIGH_BOUND = 15;
 
-      final int SIZEOFARRAY = 75;
-      final int HIGHBOUND = 15;
+      int target = Integer.MIN_VALUE;
+      int inputCounter = 0;
 
-      int[] nums= getRandomArray(SIZEOFARRAY, HIGHBOUND);
+      List<Integer>  locations;
 
-      List<Integer> locations = findIndexes(nums, TARGET);
-      ArrayPrintUtils.printArray(nums);
-      System.out.println();
-      System.out.println("Value " + TARGET + "found at indexes: ");
-      for (int i : locations)
+      int[] nums = getRandomArray(SIZE_OF_ARRAY, HIGH_BOUND);
+      Scanner scanner = new Scanner(System.in);
+
+      List<Integer> numsList= getRandomArrayList(SIZE_OF_ARRAY, HIGH_BOUND);
+
+      do
       {
-         System.out.println(i + " ");
+         inputCounter++;
+         if (inputCounter > 1)
+         {
+            System.out.print("Invalid input. ");
+         }
+         System.out.println(
+               "Enter an integer to be found between 1 and "
+               + HIGH_BOUND + ".");
+         try
+         {
+            target = Integer.parseInt(scanner.nextLine());
+         }
+         catch (NumberFormatException e)
+         {
+            System.out.println(e.getMessage());
+         }
+      }while (target < 1 || target > HIGH_BOUND );
+
+      //array test
+      try
+      {
+         locations = findIndexes(nums, target);
+         ArrayPrintUtils.printArray(nums);
+         System.out.println();
+         System.out.println("Value " + target + " found in random array "
+               + locations.size() + " times at indexes: ");
+         locations.forEach(loc ->
+         {
+            System.out.print(loc);
+            System.out.print(" ");
+         });
+         System.out.println();
       }
+      catch (TargetNotFoundException e)
+      {
+         System.out.println(e.getMessage());
+      }
+
+      //arrayList test
+      try
+      {
+         locations = findIndexes(numsList, target);
+         ArrayPrintUtils.printArray(nums);
+         System.out.println();
+         System.out.println("Value " + target + " found in random List "
+               + locations.size() + " times at indexes: ");
+         locations.forEach(loc ->
+         {
+            System.out.print(loc);
+            System.out.print(" ");
+         });
+         System.out.println();
+      }
+      catch (TargetNotFoundException e)
+      {
+         System.out.println(e.getMessage());
+      }
+   }
+}
+
+class TargetNotFoundException extends Exception
+{
+   TargetNotFoundException(String message)
+   {
+      super(message);
    }
 }
